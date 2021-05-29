70a483c6f6669653b4c167ab464c27e2bb659f38
Harshit Verma <Harshit.Verma@team.telstra.com>
2021-05-30 02:49:38 +0530

    fixed cart items' order issue

--- src/Components/Cart.js
+++ src/Components/Cart.js
@@ -1,50 +1,53 @@
 import React, { useState, useEffect } from "react";
 import "./Cart.css";
 
+let cartId = 0;
 const Cart = (props) => {
   const [cartList, setCartList] = useState([]);
 
-  useEffect(() => {
+  const findCartItems = (cart) => {
     const myList = [];
-    let localCart = JSON.parse(localStorage.getItem("cartItems"));
-    let initialCart = localCart ? localCart : [];
-    initialCart.forEach(id => {
-      for(let i=0;i<props.productsList.length;i++) {
-        if(id === props.productsList[i].id.toString())
+    cart.forEach((id) => {
+      for (let i = 0; i < props.productsList.length; i++) {
+        if (id === props.productsList[i].id.toString())
           myList.push(
-            <div className="card">
-              <div style={{ marginLeft: "8px" }}>{props.productsList[i].name}</div>
+            <div key = {++cartId} className="card">
+              <div style={{ marginLeft: "8px" }}>
+                {props.productsList[i].name}
+              </div>
             </div>
           );
       }
     });
+    return myList;
+  };
+
+  useEffect(() => {
+    let localCart = JSON.parse(localStorage.getItem("cartItems"));
+    let initialCart = localCart ? localCart : [];
+    const myList = findCartItems(initialCart);
     setCartList(myList);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [props.productsList]);
 
   useEffect(() => {
-    const myList = [];
     let localCart = JSON.parse(localStorage.getItem("cartItems"));
     let initialCart = localCart ? localCart : [];
-    props.productsList.forEach((product) => {
-      if (props.cart.includes(product.id.toString())) {
-        myList.push(
-          <div className="card">
-            <div style={{ marginLeft: "8px" }}>{product.name}</div>
-          </div>
-        );
-      }
-    });
+    const myList = findCartItems(props.cart);
     setCartList((prev) => [...prev, ...myList]);
-    localStorage.setItem(
-      "cartItems",
-      JSON.stringify([...initialCart, ...props.cart])
-    );
+    localStorage.setItem("cartItems", JSON.stringify([...initialCart, ...props.cart]));
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [props.cart]);
 
   const renderCartList = () => {
     if (cartList.length === 0)
       return <div className="carts-list empty-cart">Your cart is empty</div>;
-    else return <div className="carts-list">{cartList}</div>;
+    return <div className="carts-list">{cartList}</div>;
+  };
+
+  const checkout = () => {
+    setCartList([]);
+    localStorage.setItem("cartItems", "[]");
   };
 
   return (
@@ -54,7 +57,7 @@ const Cart = (props) => {
         <div>{cartList.length} items</div>
       </div>
       {renderCartList()}
-      <button className="checkout-button" onClick={() => setCartList([])}>
+      <button className="checkout-button" onClick={checkout}>
         Checkout --&gt;
       </button>
     </div>
